9af42074a9d8b436e821ed4eda200411c58cbfec
Kevin Towe <kevintowe27@gmail.com>
2017-09-10 01:36:36 -0600

    done moving error component to the shared module

---
+++ src/app/shared/error-dialog/error-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MdDialog, MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-error-dialog',
+  templateUrl: './error-dialog.component.html',
+  styleUrls: ['./error-dialog.component.css']
+})
+export class ErrorDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MdDialogRef<ErrorDialogComponent>,
+    @Inject(MD_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
